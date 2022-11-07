d2b566a3bfe81f02b38ba017a026675e6800852f
saharmoradik <74436199+saharmoradik@users.noreply.github.com>
2022-11-07 16:03:59 +0100

    First commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="max-w-full">
+    <NuxtWelcome />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,5 @@
+import { defineNuxtConfig } from "nuxt/config";
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  buildModules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+});
